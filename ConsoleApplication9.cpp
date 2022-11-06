4fa40e6ed41290f445f17b93e546b0576c150c58
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-06 15:52:18 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,36 @@
+﻿// Чертоусова дз Задача 1
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    
+    const int size = 100;
+     int n;
+     cin >> n;
+     int mass[size];;
+     for (int i = 0; i != n; i++)
+     {
+         cin >> mass[i];
+
+         if ((mass[i]%2)!=0) {
+              mass[i]  = 0;
+             
+         }
+         
+     }
+    
+   
+     for (int i = 0; i != n; i++) {
+
+
+         if (mass[i] < 0) {
+             mass[i] *= -1;
+            
+         }
+     }
+     for (int i = 0; i != n; i++) cout << mass[i];
+}
+
+
